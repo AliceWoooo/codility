c5626e1cc2edc6c2381514d9b976b35a5a16e506
AliceWoooo <alicewoo358@gmail.com>
2017-12-01 18:40:11 +0000

    solution for binary gap

---
+++ BinaryGap/Solution.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+class Solution { 
+	
+	public int solution(int N) {
+		
+		String binary = Integer.toBinaryString(N);
+        int max = 0;
+        int count = 0;
+        for(int i=0; i<binary.length(); i++) {
+            char c = binary.charAt(i);
+            if(c=='1') {
+                if(count>max) {
+                    max = count;
+                }
+                count=0;
+            }
+            else if(c=='0') {
+                count++;
+            }
+        }
+        return max;
+	}
+	
+}
